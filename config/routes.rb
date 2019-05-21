4e304df13408fc5f6d772aa6f30eb01c13aec2e1
Maxence Poncelet <maxenceponcelet@hotmail.com>
2019-05-21 14:46:44 +0200

    MisterCocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :cocktails, only: %i[index show new create] do
+  resources :cocktails, only: %i[index show new create destroy] do
     resources :doses, only: %i[create new]
   end
   resources :doses, only: :destroy
